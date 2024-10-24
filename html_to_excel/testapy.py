0fee144e3ea905e099b3c82fa4721d870e75c540
JMEsquerdo <jmmesquerdo@gmail.com>
2024-10-24 09:06:03 +0200

    working with translator as well

--- html_to_excel/testapy.py
+++ html_to_excel/testapy.py
@@ -1,64 +1,54 @@
 import os
-import json
+import csv
 import chardet
 from deep_translator import GoogleTranslator
 from docx import Document
+
 
 # Detectar la codificación del archivo
 def detect_encoding(file_path):
     with open(file_path, 'rb') as f:
         raw_data = f.read(10000)  # Leer los primeros 10,000 bytes
-    result = chardet.detect(raw_data)
-    return result['encoding']
+        result = chardet.detect(raw_data)
+        return result['encoding']
 
-# Ruta del archivo JSON y la plantilla de Word
-json_filepath = os.path.join(os.path.dirname(__file__), "./data.json")
+
+# Ruta del archivo CSV y la plantilla de Word
+csv_filepath = os.path.join(os.path.dirname(__file__), "./combined_output.csv")
 template_filepath = os.path.join(os.path.dirname(__file__), "./template.docx")
 
-# Detectar la codificación del archivo JSON
-encoding = detect_encoding(json_filepath)
+
+# Detectar la codificación del archivo CSV
+encoding = detect_encoding(csv_filepath)
 print(f"Codificación detectada: {encoding}")
 
-# Leer el archivo JSON con la codificación detectada
-with open(json_filepath, 'r', encoding=encoding) as i18n_file:
-    parsed_json = json.load(i18n_file)
+
+# Leer el archivo CSV con la codificación detectada
+with open(csv_filepath, 'r', encoding=encoding) as csv_file:
+    reader = csv.DictReader(csv_file)
+    data = [row for row in reader]
+
 
 # Traductor
 translator = GoogleTranslator(source='auto', target='es')
 
+
 # Cargar el documento Word existente como plantilla
 doc = Document(template_filepath)
 
-# Verificar si parsed_json es un diccionario o una lista
-if isinstance(parsed_json, dict) and "Results" in parsed_json:
-    results = parsed_json["Results"]
-    for group in results.values():
-        for original in group:
-            doc.add_heading(original['GroupName'], level=2)  # Usar el nombre del grupo como título 2
-            
-            for control in original.get('Controls', []):
-                for field, value in control.items():
-                    translated_title = translator.translate(field)
-                    doc.add_heading(translated_title, level=3)  # Añadir como título 3 en el documento
-                    translated_content = translator.translate(value)
-                    doc.add_paragraph(translated_content)
 
-elif isinstance(parsed_json, list):
-    for original in parsed_json:
-        doc.add_heading(original['FindingName'], level=2)  # Usar el nombre del hallazgo como título 2
-        
-        for field, value in original.items():
-            # Ignorar el campo ID y el FindingName
-            if field == 'ID' or field == 'FindingName':
-                continue
-            
-            translated_title = translator.translate(field)
-            doc.add_heading(translated_title, level=3)  # Añadir como título 3 en el documento
-            translated_content = translator.translate(value)
-            doc.add_paragraph(translated_content)
+# Traducir y agregar contenido al documento
+for row in data:
+    for key, value in row.items():
+        translated_title = translator.translate(key)
+        doc.add_heading(translated_title, level=3)  # Añadir como título 3 en el documento
+        translated_content = translator.translate(value)
+        doc.add_paragraph(translated_content)
+
 
 # Guardar el documento respetando la plantilla original
 output_filepath = os.path.join(os.path.dirname(__file__), "traducciones_respetando_plantilla.docx")
 doc.save(output_filepath)
+
 
 print(f"Documento guardado en: {output_filepath}")
