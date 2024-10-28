7fdbb248cb2de228933315d55a32e3babc734468
JMEsquerdo <jmmesquerdo@gmail.com>
2024-10-28 09:22:59 +0100

    csv translate

--- html_to_excel/word_processor.py
+++ html_to_excel/word_processor.py
@@ -8,6 +8,7 @@ from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.oxml.ns import qn
 from docx.oxml import OxmlElement
 from translator import Translator
+import pandas as pd
 
 
 def detect_encoding(file_path):
@@ -17,7 +18,7 @@ def detect_encoding(file_path):
     return result['encoding']
 
 
-def process_word_document(csv_filepath, template_filepath, output_filepath):
+def process_word_document(csv_filepath, template_filepath, output_filepath, output_csv_filepath):
     # Detectar la codificación del archivo CSV
     encoding = detect_encoding(csv_filepath)
 
@@ -33,34 +34,26 @@ def process_word_document(csv_filepath, template_filepath, output_filepath):
     doc = Document(template_filepath)
 
     # Traducir y agregar los campos
+    translated_data = []
     for row in data:
-
-        # Traducir el título y agregar como H2
-        translated_title = translator.translate(row['Title'])
-        doc.add_heading(translated_title, level=2)
-
+        translated_row = {}
         for key, value in row.items():
+            translated_key = translator.translate(key)
+            translated_value = translator.translate(value)
+            translated_row[translated_key] = translated_value
+            # Agregar los campos al documento Word
             if key != 'Title':
-                translated_key = translator.translate(key)
-                translated_value = translator.translate(value)
-            
                 paragraph = doc.add_paragraph()
                 paragraph.alignment = WD_ALIGN_PARAGRAPH.LEFT
-            
                 run = paragraph.add_run(translated_key)
                 font = run.font
                 font.bold = True
-            
-                # Agregar los puntos y el salto de línea
                 run = paragraph.add_run(":")
                 paragraph.add_run().add_break()
-            
-                # Agregar el contenido del campo
                 run = paragraph.add_run(translated_value)
                 font = run.font
                 if key == 'RiskRating':
                     font.bold = True
-                
                     # Set font color
                     if translated_value == 'Crítico':
                         font.color.rgb = RGBColor(139, 0, 0)  # Dark red
@@ -72,6 +65,13 @@ def process_word_document(csv_filepath, template_filepath, output_filepath):
                         font.color.rgb = RGBColor(0, 128, 0)  # Green
                     elif translated_value == 'Informativo':
                         font.color.rgb = RGBColor(0, 0, 255)  # Blue
+        translated_data.append(translated_row)
+
+    # Crear un DataFrame con el texto traducido
+    df = pd.DataFrame(translated_data)
+
+    # Escribir el DataFrame en un archivo CSV
+    df.to_csv(output_csv_filepath, index=False)
 
     # Guardar el documento respetando la plantilla original
     doc.save(output_filepath)
@@ -81,9 +81,10 @@ if __name__ == '__main__':
     csv_filepath = os.path.join(os.path.dirname(__file__), "./combined_output.csv")
     template_filepath = os.path.join(os.path.dirname(__file__), "./template.docx")
     output_filepath = os.path.join(os.path.dirname(__file__), "traducciones_respetando_template.docx")
-    
+    output_csv_filepath = os.path.join(os.path.dirname(__file__), "translated_text.csv")
+
     print("Word translation started")
 
-    process_word_document(csv_filepath, template_filepath, output_filepath)
+    process_word_document(csv_filepath, template_filepath, output_filepath, output_csv_filepath)
 
     print("Word translation finished")
